644a5f1d3bd8ffcc49ef84492cf11c45cf11a2ea
Chao Xu <xux1912@gmail.com>
2020-07-31 18:22:49 -0500

    Update DL_activation_function.R

--- DL_activation_function.R
+++ DL_activation_function.R
@@ -1,8 +1,9 @@
-library(ggplot2)
 x=seq(-10,10,by=0.05)
 alpha=1.67326324
 lambda=1.05070098
 y=x
+df3=data.frame(x,y,gp="Linear")
+
 for(i in 1:length(x)){
 if(x[i]>0){
 	y[i]=lambda*x[i]
@@ -11,6 +12,7 @@ if(x[i]>0){
 }
 }
 df1=data.frame(x,y,gp="SELU")
+
 
 for(i in 1:length(x)){
 if(x[i]>0){
@@ -21,9 +23,12 @@ if(x[i]>0){
 }
 df2=data.frame(x,y,gp="ReLU")
 
-df=rbind(df1,df2)
+
+df=rbind(df1,df2,df3)
 ggplot(data=df,aes(x=x,y=y, group=gp))+
-geom_line(aes(color=gp), size=1.25)+
+geom_line(aes(color=gp, linetype=gp), size=1.25)+
 scale_color_brewer(name="activation function",palette="Dark2")+
+scale_linetype_manual(name="activation function",values=c("solid", "solid", "twodash"))+
 theme_classic()+
-theme(text = element_text(size=20), legend.position = c(0.22, 0.9))
+theme(text = element_text(size=20), legend.position = c(0.22, 0.88),
+axis.title.y = element_text(angle = 0, vjust=0.5))
