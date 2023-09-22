bcc7916972836ab98112de7c3f5372b31e29ac34
Chao Xu <xux1912@gmail.com>
2023-09-21 22:24:51 -0500

    Update table1.R

--- table1.R
+++ table1.R
@@ -2,6 +2,7 @@ library(table1)
 library(ggplot2)
 
 d=read.table("demographic_data.txt", header=T, sep="\t", na.strings = "")
+##Recode data
 d$PageTime=d$Time.since.last.known.normal..mins. / 60
 d$Stroke="Y"
 d$Stroke[is.na(d$Type.of.Stroke)]="N"
@@ -15,75 +16,73 @@ d$AIS.Training=factor(d$AIS.Training..1.yes..0.no., levels=c(0,1), labels=c("N",
 d$AIS.Training_pvalue=factor(d$AIS.Training..1.yes..0.no., levels=c(0,1,2), labels=c("N", "Y", "P-value"))
 d$mRS=factor(d$mRS.baseline, levels=c(0,1,2,3,4,5), labels=c("0", "1", "2", "3", "4", "5"))
 
+##change the display name of the variabe in table
 label(d$PageHour)="Page Hour"
 label(d$AIS.Training)="AIS Training"
 label(d$AIS.Training_pvalue)="AIS Training"
 
-my.render.cont <- function(x) {
-    with(stats.apply.rounding(stats.default(x), digits=2), c("",
-        "Mean (SD)"=sprintf("%s (&plusmn; %s)", MEAN, SD), "Median [Q1, Q3]"=sprintf("%s [%s, %s]", MEDIAN, q25,q75)))
-}
-
-table1(~Age + Sex + PageHour + SystemsCategory + Neuro.Experience + Days.since.admit + AIS.Training + DM + HTN + CAD + Afib + FocalvsNon.focalsymptoms +
-		Dyslipidemia + Current.Smoking + Prior.stroke + NIHSS + mRS +Type.of.Stroke + Stroke, data=d, render.continuous=my.render.cont)
- 
-
+##rndr defines the testing method for each variable by d$Group_pvalue. Notice d is the data frame. In other words, the dataframe name must be d.
+##d$Group_pvalue is also a fixed name.
 rndr <- function(x, name, ...) {
     if (length(x) == 0) {
         y <- d[[name]]
         s <- rep("", length(render.default(x=y, name=name, ...)))
         if (is.numeric(y)) {
-            p <- t.test(y ~ d$AIS.Training_pvalue)$p.value
-	    p = summary(aov(y ~ droplevels(d$TYPE_pvalue)))[[1]][1,5]
+		if(shapiro.test(y)$p.value<0.05){
+			if(length(levels(droplevels(d$Group_pvalue)))>2){
+            		p <- kruskal.test(y ~ droplevels(d$Group_pvalue))$p.value
+			}else{
+				p <- wilcox.test(y ~ droplevels(d$Group_pvalue))$p.value
+			}
+			p=paste(scales::pvalue(p),"**",sep="")
+		}else{
+			if(length(levels(droplevels(d$Group_pvalue)))>2){
+				p = summary(aov(y ~ droplevels(d$Group_pvalue)))[[1]][1,5]
+			}else{
+				p = t.test(y ~ droplevels(d$Group_pvalue))$p.value
+			}
+			p=scales::pvalue(p)
+		}
         } else {
-            p <- chisq.test(table(y, droplevels(d$AIS.Training_pvalue)))$p.value
+		yd=table(y, droplevels(d$Group_pvalue))
+		if(sum(yd<5)/length(yd)>0.2 | sum(yd==0)>0){
+			p <- fisher.test(table(y, droplevels(d$Group_pvalue)))$p.value
+			p=paste(scales::pvalue(p),"*",sep="")
+		}else{
+            	p <- chisq.test(table(y, droplevels(d$Group_pvalue)))$p.value
+			p=scales::pvalue(p)
+		}
         }
-        s[2] <- sub("<", "&lt;", format.pval(p, digits=3, eps=0.001))
-        s
-    } else {
-        render.default(x=x, name=name, ...)
-    }
-}
-rndr_fisher <- function(x, name, ...) {
-    if (length(x) == 0) {
-        y <- d[[name]]
-        s <- rep("", length(render.default(x=y, name=name, ...)))
-        if (is.numeric(y)) {
-            p <- t.test(y ~ d$AIS.Training_pvalue)$p.value
-        } else {
-            p <- fisher.test(table(y, droplevels(d$AIS.Training_pvalue)))$p.value
-        }
-        s[2] <- sub("<", "&lt;", format.pval(p, digits=3, eps=0.001))
-        s
-    } else {
-        render.default(x=x, name=name, ...)
-    }
-}
-
-rndr_fisher_simul <- function(x, name, ...) {
-    if (length(x) == 0) {
-        y <- d[[name]]
-        s <- rep("", length(render.default(x=y, name=name, ...)))
-        if (is.numeric(y)) {
-            p <- t.test(y ~ d$AIS.Training_pvalue)$p.value
-        } else {
-            p <- fisher.test(table(y, droplevels(d$AIS.Training_pvalue)), simulate.p.value=TRUE)$p.value
-        }
-        s[2] <- sub("<", "&lt;", format.pval(p, digits=3, eps=0.001))
+        s[2] <- sub("<", "&lt;", p)
         s
     } else {
         render.default(x=x, name=name, ...)
     }
 }
 
+##default table1 function
 rndr.strat <- function(label, n, ...) {
     ifelse(n==0, label, render.strat.default(label, n, ...))
 }
 
+##define what to show for continuous varialbe. here is Mean SD and Median Q1 Q3
+my.render.cont <- function(x) {
+    with(stats.apply.rounding(stats.default(x), digits=2), c("",
+        "Mean (SD)"=sprintf("%s (&plusmn;%s)", MEAN, SD), "Median [Q1, Q3]"=sprintf("%s [%s, %s]", MEDIAN, q25,q75)))
+}
 
-table1(~Time.to.CT.MR.from.stroke.alert..min. + Time.to.Neurology.eval | AIS.Training_pvalue, data=d, droplevels=F, render=rndr, render.strat=rndr.strat, overall=F)
-table1(~IV.tPA + Time.to.IV.Tpa.from.last.known.normal + Time.to.IV.tPA.from.stroke.alert + Final.Diagnosis | AIS.Training_pvalue, data=d, droplevels=F, render=rndr_fisher, render.strat=rndr.strat, overall=F)
+##Define the group/column variable in Table 1. 
+d$Group=factor(d$Treatment, levels=c("Tr1", "Tr2", "Tr3"), labels=c("Tr1", "Tr2", "Tr3"))
 
-table1(~PageHour  + DM + HTN + CAD + Afib +
-		Dyslipidemia + Current.Smoking + Prior.stroke + NIHSS + mRS +Type.of.Stroke + Stroke | AIS.Training_pvalue, 
-		data=d, droplevels=F, render=rndr, render.strat=rndr.strat, overall=F)
+##Define the group/column variable in Table 1, when a p-value column is needed.
+d$Group_pvalue=factor(d$Treatment, levels=c("Tr1", "Tr2", "Tr3", "P-value"), labels=c("Tr1", "Tr2", "Tr3", "P-value"))
+
+##This is the Table 1 working function. No p-value/comparion among column variable.
+table1(~Age + Sex + DM + HTN + CAD + Afib + BMI +
+		Dyslipidemia + Current.Smoking, data=d, render.continuous=my.render.cont)
+
+##This is the Table 1 working function, with p-value/comparion among column variable.
+table1(~Age + Sex + DM + HTN + CAD + Afib + BMI +
+		Dyslipidemia + Current.Smoking | Group_pvalue, data=d, droplevels=F,
+		render.continuous=my.render.cont, render.strat=rndr.strat, overall=T, render=rndr)
+
